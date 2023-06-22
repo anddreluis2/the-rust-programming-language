8e64db74c2ec29978c192ce7da929c2eb2a40180
anddreluis2 <anddreluis98@gmail.com>
2023-06-21 21:03:36 -0300

    installing cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-78ebc6ae835d4124.d
@@ -0,0 +1,5 @@
+/home/andre/projects/rust/hello_cargo/target/debug/deps/hello_cargo-78ebc6ae835d4124.rmeta: src/main.rs
+
+/home/andre/projects/rust/hello_cargo/target/debug/deps/hello_cargo-78ebc6ae835d4124.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/andre/projects/rust/hello_cargo/target/debug/hello_cargo: /home/andre/projects/rust/hello_cargo/src/main.rs
